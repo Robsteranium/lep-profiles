fa90e0a985098fb4492a724cb63345a4c64ce9c6
Robin Gower <robin.git@infonomics.ltd.uk>
2013-09-10 14:59:10 +0100

    Axes on histogram

--- totals_hist.js
+++ totals_hist.js
@@ -4,7 +4,7 @@ function totals_histogram(data) {
 
   "use strict";
   var container_dimensions = {width: 500, height: 250},
-      margins = {top: 20, right: 20, bottom: 20, left: 20},
+      margins = {top: 0, right: 0, bottom: 10, left: 80},
       chart_dimensions = {
         width: container_dimensions.width - margins.left - margins.right,
         height: container_dimensions.height - margins.top - margins.bottom
@@ -18,6 +18,10 @@ function totals_histogram(data) {
     .domain(d3.extent(data.map( function(d) {return d.total;} )))
     .range([chart_dimensions.height,0]);
 
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left");
+
   var histogram = d3.select("#histogram")
       .append("svg")
         .attr("width", container_dimensions.width)
@@ -25,6 +29,30 @@ function totals_histogram(data) {
       .append("g")
         .attr("transform", "translate(" + margins.left + "," + margins.top + ")")
         .attr("id","histogram");
+
+  // Axes
+  histogram.append("text")
+      .attr("x", chart_dimensions.width - margins.right - 220)
+      .attr("y", chart_dimensions.height - margins.bottom - 80)
+      .text("LEP areas in productivity order");
+
+  var yAxisG = histogram.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
+
+  yAxisG.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", -margins.left)
+      .attr("dy", "1em")
+      .style("text-anchor", "end")
+      .text("Total Local Productivity");
+
+  yAxisG.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", -margins.left)
+      .attr("dy", "2.5em")
+      .style("text-anchor", "end")
+      .text("Gain/ Loss (£ per hour)");
 
   // Background Bars
   var back_bars = histogram.selectAll(".hist_back")
